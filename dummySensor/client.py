e199edf0d61680eaed62b77930a01eee2d7c19e4
unknown <sb@Stephen-PC.internal.audioanalytic.com>
2015-08-12 12:53:55 +0100

    dummySensor now receives commands
    Wrong ibmiotf class had been used- changed to use device API (rather than application).
    
    Follow through changes based on this change also included.

--- dummySensor/client.py
+++ dummySensor/client.py
@@ -1,33 +1,57 @@
 import time
-import os, json
-import ibmiotf.application
+import json
+import ibmiotf.device
 from datetime import datetime
+import uuid
 
 client = None
+interval = 5
+cmdCount = 0
+evCount = 0
 
-#def myCommandCallback(cmd):
-#	if cmd.event == "light":
-#		print command
+def myCommandCallback(cmd):
+	try:
+		global cmdCount, interval
+		
+		if cmd.command == "setInterval":
+			if 'interval' not in cmd.data:
+				print("Error - command is missing require parameter: 'interval'")
+			else:
+				 interval = cmd.data['interval']
+		elif cmd.command == "print":
+			if 'message' not in cmd.data:
+				print("Debug Message missing field: 'message'")
+			else:
+				print "Command received-  '%s' " %(cmd.data['message'])
+		else:
+			print "Command received: cmd -'%s' ; body -'%s'" %(cmd.command, json.dump(cmd.data))
+			print "Invalid Command"
+		cmdCount = cmdCount + 1
+	except Exception as err:
+		print "Command Callback Err - %s " %(err)
 
 try:
 	print ("Parsing Config")
-	options = ibmiotf.application.ParseConfigFile("/home/pi/dummySensor/device.cfg")
-	print "...parsed"
+	options = ibmiotf.device.ParseConfigFile("/home/pi/dummySensor/device.cfg")
 	options["deviceId"] = options["id"]
-	options["id"] = "aaa" + options["id"]
-	client = ibmiotf.application.Client(options)
+	options["id"] = options["deviceId"]
+	client = ibmiotf.device.Client(options)
 	client.connect()
-	print ("Connected...")
-#	client.deviceEventCallback = myCommandCallback
-#	client.subscribeToDeviceEvents(event="light")
+	client.commandCallback = myCommandCallback
 
 	print "Begin to Send Data..."
-	
 	while True:
-		time.sleep(20)
-		myData = str(datetime.now())
-		print "Publishing mayflyTime %s" %(myData)
-		client.publishEvent("raspberrypi", options["deviceId"], "mayflyTime", "json", myData)
-		print "Loop Fnished"
-except ibmiotf.ConnectException as e:
-	print e
+		try:
+			time.sleep(interval)
+			myQoSLevel=2
+			myData = str(datetime.now())
+			client.publishEvent("mayflyTime", "json", myData, myQoSLevel)
+			print "Publishing evCount: %d ; cmdCount: %d" %(evCount, cmdCount)
+			evCount = evCount + 1
+		except Exception as err:
+			print "failing to run"
+			print err
+	
+	client.disconnect()
+except Exception as e:
+	print "Exception - %s " %(e)
