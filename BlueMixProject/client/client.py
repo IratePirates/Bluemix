07b0e88a543c7a6211bf539b7c5cbf2ae383db76
unknown <sb@Stephen-PC.internal.audioanalytic.com>
2015-08-18 11:36:38 +0100

    Rearrange File tree to make sense

---
+++ BlueMixProject/client/client.py
@@ -0,0 +1,60 @@
+import time
+import json
+import hermod #Messenger Module -  the good messenger gods were already taken...
+
+cmdCount = 0
+interval = 20
+transmitData = False
+
+def demoCommandCallback(cmd):
+	try:
+		global cmdCount, interval
+		global transmitData
+
+		if cmd.command == "setInterval":
+			if 'interval' not in cmd.data:
+				print("Error - command is missing require parameter: 'interval'")
+			else:
+				 interval = cmd.data['interval']
+		elif cmd.command == "print":
+			if 'message' not in cmd.data:
+				print("Debug Message missing field: 'message'")
+			else:
+				print "Command received-  '%s' " %(cmd.data['message'])
+		elif cmd.command == "transmitData":
+			transmitData = True
+		else:
+			print "Command received: cmd -'%s' ; body -'%s'" %(cmd.command, json.dump(cmd.data))
+			print "Invalid Command"
+		cmdCount = cmdCount + 1
+	except Exception as err:
+		print "Command Callback Err - %s " %(err)
+
+try:
+	print "Configure Device Messenger"
+	messenger = hermod.device("./device.cfg")
+	messenger.registerCmdCallback(demoCommandCallback)
+	print "Begin to Send Data..."
+	while True:
+
+		try:
+			if transmitData:
+				messenger.fileSend("./Sample.txt")
+				transmitData = False
+			else:
+				# Additional flow control can be added with a raw_input string
+				# example: http://stackoverflow.com/questions/7255463/exit-while-loop-by-user-hitting-enter-key
+					# N.B. would require second thread for message control e.g. : http://pymotw.com/2/multiprocessing/basics.html
+
+				messenger.publishEvent(myData=time.sleep(interval), msgType="devStat")
+				# Max length of MQTT message is 4kB, currently. This will include MQTT headers(Topic, etc), IoT headers + Timestamp.
+					#topic = 'iot-2/evt/'+event+'/fmt/' + msgFormat
+					#MQTT payload = JSON encoded (data + datetime.now(pytz.timezone('UTC')))
+				#therefore send 2kB chunks + assume success.
+		except Exception as err:
+			print "failing to run"
+			print err
+
+	messenger.disconnect()
+except Exception as e:
+	print "Exception - %s " %(e)
---
+++ BlueMixProject/libs/hermod.py
@@ -0,0 +1,167 @@
+# IBM Internet of Things Communiucation Module for Device
+#        -  the good messenger gods were already taken...
+# Author- Steve B
+#!/usr/bin/python2.7
+
+import ibmiotf.application
+import ibmiotf.device
+import os
+
+class device:
+	def __init__(self, cfgFile="./device.cfg"):
+		self.client = None
+		self.evCount = 0
+
+		try:
+			print ("Parsing Config")
+			options = ibmiotf.device.ParseConfigFile(cfgFile)
+			self.client = ibmiotf.device.Client(options)
+		except Exception as e:
+			print "Exception - Initialising Device Messaging -  %s " %(e)
+
+	def defaultCommandCallback(self, cmd):
+		try:
+			print "Received command %s" % (cmd.command)
+		except Exception as e:
+			print "Exception - defaultCommandCallback Messaging %s " %(e)
+
+	def registerCmdCallback(self, myCommandCallback="defaultCommandCallback"):
+		try:
+			self.client.connect()
+			self.client.commandCallback = myCommandCallback
+		except Exception as e:
+			print "Exception - binding cmdCallback -  %s " %(e)
+
+	def publishEvent(self, myData, msgType="devStat", encoding="json", QoS=0):
+		try:
+			if self.client :
+				self.client.publishEvent(msgType, encoding, myData, QoS)
+				print "Publishing evCount: %d ;" %(self.evCount)
+				self.evCount = self.evCount + 1
+			else:
+				print "Cannot Publish Event, no Client exists yet"
+		except Exception as e:
+			print "Exception - Publishing Event -  %s " %(e)
+
+	def disconnect(self):
+		try:
+			self.client.disconnect()
+		except Exception as e:
+			print "Exception - Disconnecting -  %s " %(e)
+
+	def sendFragDataEvent(self, fileHandle, totalChunks, bufferSize = 2048):
+		dataSrc=[]
+		try:
+			#open text file
+			for chunk in range(0 , totalChunks) :
+				dataSrc.append(fileHandle.read(bufferSize))
+
+			#Send Fragments
+			for i in range(0 , totalChunks) :
+				pktNum =i+1
+
+				myData = {'pkt' : pktNum, 'maxPkt' : totalChunks, 'd':dataSrc[i]}
+				self.publishEvent( myData, msgType="dataFrag", encoding="json", QoS=2)
+
+			print "Publishing Data Fragmented as %i packets " % (totalChunks)
+			return True
+		except Exception as err:
+			print "Data Send Err - %s " %(err)
+
+	def fileSend(self, filePath, bufferSize = 2048):
+		try:
+			print "Opening File Location... "
+			fileSize = os.path.getsize(filePath)
+			totalChunks = (fileSize / bufferSize) + (fileSize % bufferSize > 0) # Hacky Ceiling Function
+			print "Opening File- %s ; size %d; \n   bufferSize %d ; chunks required - %d ;" %( filePath, fileSize, bufferSize, totalChunks)
+			sampleFile = open(filePath, 'r', bufferSize)
+			print "Data collected"
+			self.sendFragDataEvent(sampleFile, totalChunks, bufferSize)
+			sampleFile.close()
+			return True
+		except Exception as err:
+			print "fileSend Err - %s " %(err)
+
+class server:
+	def __init__ (self, configFilePath="./iotServer.cfg"):
+		self.server = None
+		self.evCount = 0
+		self.cmdCount = 0
+		self.expectingDataFlag = False
+		self.dataAvailable = False
+		self.rxData= [] #store for received data
+
+		try:
+			print "Configuring IoT..."
+
+			options = ibmiotf.application.ParseConfigFile(configFilePath)
+			self.server = ibmiotf.application.Client(options)
+		except ibmiotf.ConnectionException  as e:
+			print "Error on grabbing IoT Configuration - %s" % (e)
+		except Exception as e:
+			print "Exception - Initialising Server Messaging -  %s " %(e)
+
+	def sendCmd(self, command, payload ="",deviceId = 'b827ebddb7d1', device="raspberrypi" ):
+	#TODO - CLEAN ME up for general useage
+		try:
+			#Do all the jazz for managing multiple devices in here
+			self.server.publishCommand(device, deviceId, str(command), "json", payload)
+			print "Sending '%s' cmd, payload '%s' to device %s" % (command, payload, deviceId)
+			self.cmdCount = self.cmdCount + 1
+		except Exception as e:
+			print "Error in Cmd Callback"
+			print e
+
+	def debugCmd(self, message):
+			payload = {'message' : message}
+			self.sendCmd("print", payload)
+
+	def registerCallbackMethods(self, myEventCallback, myStatusCallback):
+		try:
+			self.server.connect()
+			self.server.deviceEventCallback = myEventCallback
+			self.server.deviceStatusCallback = myStatusCallback #why does this not work
+			self.server.subscribeToDeviceStatus()
+			self.server.subscribeToDeviceEvents()
+
+		except Exception as e:
+			print "Error in Registering Status Callback"
+			print e
+
+	def sendDataPushCmd(self, deviceId = 'b827ebddb7d1', device="raspberrypi"):
+		try:
+			#clear list of data
+			while len(self.rxData) > 0 : self.rxData.pop()
+
+			self.expectingDataFlag = True
+
+			self.sendCmd(command="transmitData", payload ="",deviceId = 'b827ebddb7d1', device="raspberrypi" )
+		except Exception as e:
+			print "Error while sending request for Data - %s" %e
+
+	def retrieveDataFromDevice(self):
+		try:
+			if  self.dataAvailable and not self.expectingDataFlag:
+				print "Data Available - Printing"
+				self.dataAvailable = False
+				return self.rxData
+			else:
+				print "No Data Available - Contacting device"
+				self.sendDataPushCmd(deviceId = 'b827ebddb7d1', device="raspberrypi")
+
+				rxDataTmp = []
+
+				if not self.expectingDataFlag:
+					#Or hack, and require the page is reloaded.
+					rxDataTmp = self.retrieveDataFromDevice()
+
+				if rxDataTmp:
+					self.dataAvailable = False
+					return rxDataTmp
+				else:
+					return 0
+		except Exception as e:
+			print "Error while Retrieving Data - %s" %e
+
+if __name__ == "__main__":
+	print "Messenger Module for communication with IBM IoT devices"
